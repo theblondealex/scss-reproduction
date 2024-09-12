a02ab3f1eb1f1351737a0745b1fb9f81709701c5
Alexander Clay <alexander.clay@smarkets.com>
2024-09-12 11:47:46 +0100

    push

--- README.md
+++ README.md
@@ -6,8 +6,11 @@
 
 1. Clone the repo
 2. Install dependencies
-3. Run the dev server
+3. CD into `./nextjs` Run the dev server
 4. Open the browser at http://localhost:3000
 5. Go to the `src/app/page.tsx` file
 6. Comment out the import of the css file and uncomment the import of the scss file
 7. You will notice that the svg is **not** rendered when using the scss file but it is rendered when using the css file
+
+8. Go back to root and cd into `./react-example` and run the dev server
+9. both scss and css files are rendered correctly
---
+++ react-example/src/App.js
@@ -0,0 +1,28 @@
+import logo from './logo.svg';
+import './App.css';
+
+// import "./page.css"; 
+import "./page.scss";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <div className={"sprites"} />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
